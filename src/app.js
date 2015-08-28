822a5d72e20d60a51d321643d111b7f0baa899fd
Anders Sjöberg <shoobah@gmail.com>
2015-08-29 00:02:55 +0200

    Stil och finess

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import todoStore from './js/todoStore'
 import Button from './components/button'
 import 'babel-core/polyfill' //Får Object.assign att lira i applikationen
+import './style/baseStyle.scss'
 
 class App extends React.Component {
   constructor(props) {
@@ -16,14 +17,20 @@ class App extends React.Component {
     );
   }
 
-  doStuff() {
+  incrementCounter() {
     todoStore.dispatch({ type: 'INCREMENT' });
+  }
+
+  decrementCounter() {
+    todoStore.dispatch({ type: 'DECREMENT' });
   }
 
   render(){
     return(
       <div>
-          <Button onClick={this.doStuff} color={'rgb(138, 148, 240)'} value={'Hit: ' + this.state.counter}/>
+          <Button onClick={this.incrementCounter} color={'rgba(78, 179, 53, 0.7)'} value={'Up'}/>&nbsp;
+          <Button onClick={this.decrementCounter} color={'rgb(240, 144, 90)'} value={'Down'}/>
+          <h1>{this.state.counter}</h1>
       </div>
     );
   }
